78489a78219a102e01e30276b4eaf82a2176757d
kamil <kamil@10.41.129.69>
2025-01-29 17:31:48 -0500

    Second Commit.

--- LSP Spring 2025 HW1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP Spring 2025 HW1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -2,8 +2,12 @@ package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 
+	/**
+	 * Kamili Campbell
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
