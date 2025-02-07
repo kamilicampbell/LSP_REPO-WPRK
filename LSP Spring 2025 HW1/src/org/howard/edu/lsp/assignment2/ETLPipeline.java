30b9b95e193bf4c904dc7044c8deb6963f891255
kamil <kamil@laptop->
2025-02-07 05:11:53 -0500

    Last commit

--- LSP Spring 2025 HW1/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
+++ LSP Spring 2025 HW1/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -21,7 +21,39 @@ public class ETLPipeline {
                 String[] fields = allLines.get(i).split(",");
                 lines.add(transform(fields));
             }
-		// TODO Auto-generated method stub
-	}
+            
+            Files.write(Paths.get(outputFile), lines);
+            System.out.println("ETL process completed!");
+        } catch (IOException e) {
+            System.out.println("Error reading or writing file: " + e.getMessage());
+        }
+    }
+	
+	private static String transform(String[] fields) {
+        String id = fields[0];
+        String name = fields[1].toUpperCase();
+        double price = Double.parseDouble(fields[2]);
+        String category = fields[3];
 
+        if (category.equals("Electronics")) {
+            price = Math.round(price * 0.9 * 100.0) / 100.0;
+        }
+        
+        if (category.equals("Electronics") && price > 500) {
+            category = "Premium Electronics";
+        }
+        
+        String priceRange;
+        if (price <= 10) {
+            priceRange = "Low";
+        } else if (price <= 100) {
+            priceRange = "Medium";
+        } else if (price <= 500) {
+            priceRange = "High";
+        } else {
+            priceRange = "Premium";
+        }
+        
+        return id + "," + name + "," + String.format("%.2f", price) + "," + category + "," + priceRange;
+    }
 }
