139f53102e99f139b0bd2545ef07571691a2a44a
kamil <kamil@laptop->
2025-02-07 02:29:04 -0500

    Second commit: attempt to add next column

--- LSP Spring 2025 HW1/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
+++ LSP Spring 2025 HW1/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -1,10 +1,27 @@
 package org.howard.edu.lsp.assignment2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+
 public class ETLPipeline {
 
 	public static void main(String[] args) {
+		String inputFile = "data/products.csv";
+        String outputFile = "data/transformed_products.csv";
+        
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(inputFile));
+            List<String> lines = new ArrayList<>();
+            lines.add("ProductID,Name,Price,Category,PriceRange");
+            
+            for (int i = 1; i < allLines.size(); i++) {
+                String[] fields = allLines.get(i).split(",");
+                lines.add(transform(fields));
+            }
 		// TODO Auto-generated method stub
-
 	}
 
 }
